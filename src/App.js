abc3d817673ae1ca8e9292718907bea6c58427c7
hosikoryutaro <hosiryuuu6424@gmail.com>
2023-02-13 18:06:40 +0900

    1回目

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export const App = () => {
+  const [omikuji, setOmikuji] = useState("引く");
+  const onClickOmikuji = () => {
+    let arr = ["大吉", "小吉", "中吉", "吉", "凶", "大凶"];
+    let index = Math.floor(Math.random() * arr.length);
+    setOmikuji(arr[index]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p className="btn" onClick={onClickOmikuji}>
+        {omikuji}
+      </p>
+    </>
   );
-}
+};
 
 export default App;
